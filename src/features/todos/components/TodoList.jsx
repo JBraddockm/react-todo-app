b8d9aeb1c89fd3f0304c7e15321713b14f2fee03
JBraddockm <jbraddockm@gmail.com>
2024-02-21 01:27:50 +0000

    Delayed todos sorted after an item marked completed

--- src/features/todos/components/TodoList.jsx
+++ src/features/todos/components/TodoList.jsx
@@ -1,12 +1,19 @@
+import { useEffect } from 'react';
+
 import TodoItem from './TodoItem.jsx';
 
 export default function TodoList({ todos, setTodos }) {
-  const sortedTodos = todos.sort((a, b) => a.isDone - b.isDone);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setTodos(todos.slice().sort((a, b) => a.isDone - b.isDone));
+    }, 1000);
+    return () => clearTimeout(timeoutId);
+  }, [setTodos, todos]);
 
   return (
     <>
       <ul>
-        {sortedTodos.map((todo) => (
+        {todos.map((todo) => (
           <TodoItem key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
         ))}
       </ul>
