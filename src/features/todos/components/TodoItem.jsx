b5d9719a2a39f78f2890114db96c80ff03704ebd
JBraddockm <jbraddockm@gmail.com>
2024-02-21 00:36:07 +0000

    Implemented accessibility improvements

--- src/features/todos/components/TodoItem.jsx
+++ src/features/todos/components/TodoItem.jsx
@@ -18,19 +18,34 @@ export default function TodoItem({ todo, todos, setTodos }) {
     );
   }
 
+  function handleKeyDown(event, id) {
+    if (event.key === 'Enter') {
+      handleTodoComplete(id);
+    }
+  }
+
   return (
     <>
       <li>
-        <label className="hidden" aria-hidden="true">
+        <label htmlFor={`todo-${todo.id}`} className="hidden" aria-hidden="true">
           Complete Task
         </label>
-        <input className="hidden" type="checkbox" readOnly checked={todo.isDone} />
-        <div // TODO change to onClick for better user experience.
+        <input
+          id={`todo-${todo.id}`}
+          className="hidden"
+          type="checkbox"
+          readOnly
+          checked={todo.isDone}
+        />
+        <div
+          tabIndex={0} // TODO change to onClick for better user experience.
           onMouseEnter={() => setsOptionsVisible(!isOptionsVisible)}
           onMouseLeave={() => setsOptionsVisible(false)}
           className="todo-item flex h-10 cursor-pointer items-center rounded px-2 hover:bg-gray-100 dark:hover:bg-gray-900">
           <span
+            tabIndex={0}
             onClick={() => handleTodoComplete(todo.id)}
+            onKeyDown={(e) => handleKeyDown(e, todo.id)}
             className="flex h-5 w-5 items-center justify-center rounded-full border-2 border-gray-500 text-transparent">
             <TodoCompleteCheckBox />
           </span>
