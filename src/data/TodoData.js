bbb408640a64c51c94f22649889924d4c3f0b754
JBraddockm <jbraddockm@gmail.com>
2024-02-21 00:03:09 +0000

    Generated sample data.

---
+++ src/data/TodoData.js
@@ -0,0 +1,7 @@
+export default function TodoData() {
+  return [
+    { id: 1, name: 'Implement oAuth2 resource server', isDone: false },
+    { id: 2, name: 'Implement Authorization Flow in Keycloak', isDone: true },
+    { id: 3, name: 'Refactor Thymeleaf application', isDone: false }
+  ];
+}
