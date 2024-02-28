61ff144f6fdd607331bf41c0cf9784b634a16b1d
JBraddockm <jbraddockm@gmail.com>
2024-02-28 10:59:19 +0000

    Fixed colour contrast and styling

--- src/features/todos/components/TodoHeader.jsx
+++ src/features/todos/components/TodoHeader.jsx
@@ -6,9 +6,9 @@ export default function TodoHeader({ completedTodos, totalTodos }) {
       <div className="mb-6 flex flex-wrap items-center justify-between">
         <div className="flex items-center">
           <TodoHeaderLogo />
-          <h4 className="ml-3 text-lg font-semibold">{`Today's Tasks`}</h4>
+          <h2 className="ml-3 text-lg font-semibold dark:text-gray-400">{`Today's Tasks`}</h2>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center text-base font-normal text-gray-500 dark:text-gray-400">
           {completedTodos}/{totalTodos}
         </div>
       </div>
