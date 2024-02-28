030f2cfb218f82a2b747d137382d0202a45ede99
JBraddockm <jbraddockm@gmail.com>
2024-02-28 10:57:51 +0000

    Added label to todoInput for accessibility

--- src/features/todos/components/TodoForm.jsx
+++ src/features/todos/components/TodoForm.jsx
@@ -27,8 +27,12 @@ export default function TodoForm() {
         onSubmit={(e) => handleFormSubmit(e)}
         className="mt-2 flex h-8 w-full items-center rounded px-2 text-sm font-medium">
         <TodoInputIcon />
+        <label htmlFor="todoInput" className="sr-only text-gray-500 dark:text-white">
+          Add todo...
+        </label>
         <input
-          className="ml-4 h-8 flex-grow bg-transparent font-medium focus:outline-none"
+          id="todoInput"
+          className="ml-4 h-8 flex-grow bg-transparent font-medium text-gray-500 focus:outline-none dark:text-gray-400"
           type="text"
           value={todo.name}
           onChange={(e) =>
