045d06e170f14496fd98672067291e9fd9afd74b
JBraddockm <jbraddockm@gmail.com>
2024-03-04 16:45:43 +0000

    Added slide-in slide-out animations to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      animation: {
+        slideIn: 'slideIn 0.3s ease-out',
+        slideOut: 'slideOut 0.5s ease-out'
+      },
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' }
+        },
+        slideOut: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' }
+        }
+      }
+    }
   },
   plugins: []
 };
