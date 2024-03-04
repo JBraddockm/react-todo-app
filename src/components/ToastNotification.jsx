6e18ee6bba8ae901b55c974d822c4045092ae93a
JBraddockm <jbraddockm@gmail.com>
2024-03-04 21:43:41 +0000

    Refactored ToastNotification.jsx to handle different notification types

--- src/components/ToastNotification.jsx
+++ src/components/ToastNotification.jsx
@@ -1,21 +1,9 @@
-import { useEffect } from 'react';
 import { CloseIcon } from 'src/components/SVGs';
 import useToastNotificationContext from 'src/hooks/useToastNotificationContext.js';
+import cn from 'src/utils/twMerge.js';
 
 export default function ToastNotification() {
-  const { toasts, setToast } = useToastNotificationContext();
-
-  useEffect(() => {
-    const timeOutId = setTimeout(() => {
-      setToast((prevToasts) => {
-        const newToasts = [...prevToasts];
-        newToasts.pop();
-        return newToasts;
-      });
-    }, 3000);
-
-    return () => clearTimeout(timeOutId);
-  }, [setToast, toasts]);
+  const { toasts } = useToastNotificationContext();
 
   return (
     <>
@@ -23,23 +11,26 @@ export default function ToastNotification() {
         {toasts.map((toast) => (
           <div
             key={toast.id}
-            id="toast-undo"
-            className="animate-slideIn flex w-full max-w-xs items-center rounded-lg bg-white p-4 text-gray-500 shadow transition-transform duration-500 dark:bg-gray-800 dark:text-gray-400"
+            id="toast-success"
+            className="mb-4 flex w-full max-w-xs items-center rounded-lg bg-white p-4 text-gray-500 shadow dark:bg-gray-800 dark:text-gray-400"
             role="alert">
-            <div className="text-sm font-normal">{toast.message}</div>
-            <div className="ms-auto flex items-center space-x-2 rtl:space-x-reverse">
-              <button className="rounded-lg p-1.5 text-sm font-medium text-blue-600 hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-700">
-                Undo
-              </button>
-              <button
-                type="button"
-                className="-mx-1.5 -my-1.5 ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white"
-                data-dismiss-target="#toast-undo"
-                aria-label="Close">
-                <span className="sr-only">Close</span>
-                <CloseIcon />
-              </button>
+            <div
+              className={cn(
+                `inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200`,
+                toast.style
+              )}>
+              {toast.icon}
+              <span className="sr-only">{`${toast.type} icon`}</span>
             </div>
+            <div className="ms-3 text-sm font-normal">{toast.message}</div>
+            <button
+              type="button"
+              className="-mx-1.5 -my-1.5 ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white"
+              data-dismiss-target="#toast-success"
+              aria-label="Close">
+              <span className="sr-only">Close</span>
+              <CloseIcon />
+            </button>
           </div>
         ))}
       </div>
